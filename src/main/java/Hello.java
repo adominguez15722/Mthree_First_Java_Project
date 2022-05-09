2f981891b19eacc27c07c419d9f2b107bfa525c5
Anthony <adominguez15722@gmail.com>
2022-05-09 09:39:12 -0500

    Initial Commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * @author Anthony
+ * email: adominguez15722@gmail.com
+ * date: 05/09/2022
+ * purpose: My first Hello World program in NetBeans IDE
+ */
+package com.mycompany.helloworld;
+        
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Anthony!");
+    }
+    
+}
